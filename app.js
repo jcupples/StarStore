2fafb1f2828ce81c5c01822bfb0508823f60b22b
Jonathan C <jcc064@lionmail.lindenwood.edu>
2016-08-02 15:14:30 -0500

    Star Store
    Bugs to work out, will be adding functionality as time goes on.

---
+++ app.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    var app = angular.module('store', []);
+
+    app.controller('StoreController', function () {
+        this.products = stars;
+    });
+    var stars = [
+        {
+            name: 'Yellow Star',
+            price: 5.00,
+            description: 'An ordinary sun-like star capable of supporting life under the right conditions',
+            canPurchase: true,
+            soldOut: true
+        },
+        {
+            name: 'Red Giant',
+            price: 3.00,
+            description: "An old star near the end of its life",
+            canPurchase: false,
+
+        }
+    ];
+
+})();
