93f065f68450ea72e119e4321dc36af35df3f963
Jonathan C <jcc064@lionmail.lindenwood.edu>
2016-08-02 20:16:11 -0500

    Simple functioning app

--- app.js
+++ app.js
@@ -2,23 +2,23 @@
     var app = angular.module('store', []);
 
     app.controller('StoreController', function () {
-        this.products = stars;
+        this.product = star;
     });
-    var stars = [
+    var star =
         {
             name: 'Yellow Star',
-            price: 5.00,
+            price: 5.99,
             description: 'An ordinary sun-like star capable of supporting life under the right conditions',
             canPurchase: true,
             soldOut: true
-        },
-        {
+        }
+ /*       {
             name: 'Red Giant',
             price: 3.00,
             description: "An old star near the end of its life",
             canPurchase: false,
 
-        }
-    ];
+        }*/
+   
 
 })();
