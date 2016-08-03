ac744131be036effb649a8469e9947ee1dbeb642
Jonathan C <jcc064@lionmail.lindenwood.edu>
2016-08-02 20:32:20 -0500

    Functioning app
    Simple functioning app for demonstration purposes. More functionality
    will be added in the coming days as I learn.

--- app.js
+++ app.js
@@ -2,23 +2,27 @@
     var app = angular.module('store', []);
 
     app.controller('StoreController', function () {
-        this.product = star;
+        this.products = stars;
     });
-    var star =
+    var stars = [
         {
             name: 'Yellow Star',
             price: 5.99,
-            description: 'An ordinary sun-like star capable of supporting life under the right conditions',
+            description: 'An ordinary sun-like star capable of supporting life under the right conditions.',
             canPurchase: true,
             soldOut: true
-        }
- /*       {
+        },
+        {
             name: 'Red Giant',
-            price: 3.00,
-            description: "An old star near the end of its life",
-            canPurchase: false,
-
-        }*/
-   
-
+            price: 2.99,
+            description: "An old star near the end of its life.",
+            canPurchase: true,
+        },
+        {
+            name: 'Pulsar',
+            price: .99,
+            description: "A dead star that spins on its axis many times a second.",
+            canPurchase: true
+        }
+    ];
 })();
